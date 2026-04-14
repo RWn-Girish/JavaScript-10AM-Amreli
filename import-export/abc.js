a60ab61e698777a743d816920a60c665ba175f07
Girish Gondaliya <rw3.girish.gk@gmail.com>
2026-04-14 11:14:59 +0530

    import-export

---
+++ import-export/abc.js
@@ -0,0 +1,37 @@
+// export => default & named export
+
+// function sayGreeting(){
+//     console.log('Hello, Good Morning');
+// }
+
+// let a = 10, b = 20;
+
+// default export
+// module.exports = sayGreeting;
+// module.exports = [a, b];
+
+
+// const addition = (a, b) => {
+//     let total = a + b;
+//     return total;
+// }
+
+// module.exports = addition;
+
+// Named exports
+
+// exports.product = (x, y) => x * y;
+
+// exports.square = (a) => a * a;
+
+
+/* ======================================================================================= */
+// ES6 Version
+
+// default exports
+// const addition = (a ,b) => a + b;
+
+// export default addition;
+
+// Named exports
+export const product = (x, y) => x * y;
---
+++ import-export/script.js
@@ -0,0 +1,28 @@
+// import  => ES5 & ES6
+
+// const sayGreeting = require('./abc');
+
+// sayGreeting();
+
+// const hello = require('./abc');
+
+// console.log(hello(11, 22));
+// console.log(hello(100, 200));
+
+// const { product, square } = require('./abc');
+
+// console.log(product(12, 6))
+// let ans = square(20);
+
+// console.log(ans);
+
+
+/* ========================================= */
+// ES6 Version
+// import addition from "./abc.js";
+
+// console.log(addition(11, 22));
+
+import { product } from "./abc.js";
+
+console.log(product(10,5));
